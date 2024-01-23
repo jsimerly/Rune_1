365c8ac78591c26e571cbb1ba6a2df448ecab6a0
Jacob <simerly81@gmail.com>
2024-01-22 23:15:26 -0500

    new GUI and updates for drafting state

--- client/client_state_manager.py
+++ client/client_state_manager.py
@@ -1,22 +1,20 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 from mouse_inputs import MouseInput
 from in_game.client_state import InGameState
 from drafting.client_state import DraftingState
 from home_screen.client_state import HomeScreenState
 from client_state_proto import ClientState
 
-
 if TYPE_CHECKING:
     import pygame as pg
 
-
 class ClientStateManager:
-    def __init__(self) -> None:
-        self.current_state: ClientState = HomeScreenState()
+    def __init__(self, set_user: Callable) -> None:
+        self.current_state: ClientState = HomeScreenState(set_user)
 
-    def start_draft(self):
-        data = self.current_state.on_exit()
+    def start_draft(self, data):
+        self.current_state.on_exit()
         self.current_state = DraftingState(**data)
         self.current_state.on_enter()
 
